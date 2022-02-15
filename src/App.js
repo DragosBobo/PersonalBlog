feb7c865d0b0b55f8fba159a7848111652671ec0
Dragos Daniel <dragos.boboluta@yahoo.com>
2022-02-15 14:39:33 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
-
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="app">
       Hello this is my blog :) , My name is Dragos :) 
+      <Navbar />
+      {/*  */}
     </div>
   );
 }
