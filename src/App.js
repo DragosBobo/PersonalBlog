7d856cbf9fe3ab7e26603a804f66efbe7a1d2ea5
Dragos Daniel <dragos.boboluta@yahoo.com>
2022-02-15 17:15:00 -0800

    8th

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Navbar from './Navbar';
-import Card from './Card';
+
 function App() {
   return (
     <div className="app">
-       
-      <Card/>
+      
+      <Navbar/>
+    
       
     </div>
   );
