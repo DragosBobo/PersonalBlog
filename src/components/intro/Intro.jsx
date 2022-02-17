45471d26e49dd53aac0ffca8299d49ab312a4132
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-02-16 20:53:20 -0800

    some home page work :)

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,7 +2,23 @@ import "./Intro.scss"
 
 function Intro() {
   return (
-    <div className="intro" id="intro"> sdasdasd</div>
+    <div className="intro" id="intro"> 
+    <div className="left">
+      <div className="imgContainer">
+        <img src="assets/man.png" alt="" />
+      </div>
+    </div>
+    <div className="right">
+      <div className="wrapper">
+        <h2>Hello , I'm </h2>
+        <h1>Dragos Daniel </h1>
+        <h3>Freelancer </h3>
+      </div>
+      <a href="#portofolio">
+        <img src="assets/down.png" alt=""/>
+      </a>
+    </div>
+    </div>
   )
 }
 
