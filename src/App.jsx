9d6cef8c7f4bff8627094b5a301e35e55248866c
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-02-16 20:02:45 -0800

    more menu opption:)

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <div className="app">
       
        <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-       <Menu/>
+       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portofolio/>
