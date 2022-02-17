b36f24a68458a10dc5f9a3dc3137ec687c23b064
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-02-16 21:11:03 -0800

    some animations on home page and then sleep :

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,16 @@
 import "./Intro.scss"
-
+import { init } from 'ityped'
+import { useEffect,useRef } from "react"
 function Intro() {
+ const textRef = useRef();
+  useEffect(()=>{
+    init(textRef.current,{
+      showCursor:true,
+      backDelay:1500,
+      strings:["Developer","Designer","Content Creator"],
+    });
+
+  },[]);
   return (
     <div className="intro" id="intro"> 
     <div className="left">
@@ -12,7 +22,7 @@ function Intro() {
       <div className="wrapper">
         <h2>Hello , I'm </h2>
         <h1>Dragos Daniel </h1>
-        <h3>Freelancer </h3>
+        <h3>Freelancer <span ref={textRef}> </span></h3>
       </div>
       <a href="#portofolio">
         <img src="assets/down.png" alt=""/>
