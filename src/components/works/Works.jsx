8221e734dbb2a460f745e024acc45ab0fa39d8bf
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-02-18 00:53:34 -0800

    some work page :)

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,7 +2,33 @@ import "./Works.scss"
 
 function Works() {
   return (
-    <div className="works" id="works"></div>
+    <div className="works" id="works">
+      <div className="slidebar">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+                <div className="leftContainer">
+                    <div className="imgContainer">
+                      <img src="assets/mobile.png" alt="mobile" />
+
+                    </div>
+                    <h2>Title</h2>
+                    <p>Lorem ipsum, dolor sit amet 
+                      consectetur adipisicing elit. Harum 
+                      facilis in eligendi aliquam odio voluptatem
+                       ipsum veniam eos, eveniet sint eum molestiae!
+                        Sint quod consequatur 
+                      laboriosam nobis sequi? Quis, in.</p>
+                      <span>Projects</span>
+                </div>
+            </div>
+            <div className="right">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
