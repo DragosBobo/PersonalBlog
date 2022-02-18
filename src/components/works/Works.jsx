1d2c4e10eba338c695b44c5e38fc663ca12e2356
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-02-18 13:38:29 -0800

    work page done :)

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,25 +1,57 @@
 import "./Works.scss"
+import {useState }from "react";
 
 function Works() {
-
-
+  
+  const[currentSlider,setCurrentSlider]=useState(0);
+  const data=[{
+    id: "1",
+    icon: "./assets/mobile.png",
+    title: "Web Design",
+    desc:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+    img:
+      "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+  },
+  {
+    id: "2",
+    icon: "./assets/globe.png",
+    title: "Mobile Application",
+    desc:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    img:
+      "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+  },
+  {
+    id: "3",
+    icon: "./assets/writing.png",
+    title: "Branding",
+    desc:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    img:
+      "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+  },
+  ];
+ 
+  const handleClick = (way)=>{
+    way === "left" ? setCurrentSlider(currentSlider > 0 ? currentSlider-1 :2) :
+    setCurrentSlider(currentSlider<data.length-1 ?  currentSlider + 1 : 0);
+    
+  };
   return (
     <div className="works" id="works">
-      <div className="slidebar">
+      <div className="slidebar" style={ {transform:`translateX(-${currentSlider * 100}vw) ` } }>
+     {data.map((d)=>(
         <div className="container">
           <div className="item">
             <div className="left">
                 <div className="leftContainer">
                     <div className="imgContainer">
-                      <img src="assets/mobile.png" alt="mobile" />
+                      <img src={d.icon} alt="mobile" />
 
                     </div>
-                    <h2>Title</h2>
-                    <p>Lorem ipsum, dolor sit amet 
-                      consectetur adipisicing elit. Harum 
-                      facilis in eligendi aliquam odio voluptatem
-                       ipsum veniam eos, eveniet sint eum molestiae!
-                        Sint quod consequatur 
+                    <h2>{d.title}</h2>
+                    <p>{d.desc}
                      </p>
                       <span>Projects</span>
                 </div>
@@ -29,10 +61,11 @@ function Works() {
                  alt="  img" />
             </div>
           </div>
-        </div>
+        </div>))}
+      
       </div>
-      <img src="assets/arrow.png" className="arrow left" alt="" />
-      <img src="assets/arrow.png" className="arrow right" alt="" />
+      <img src="assets/arrow.png" className="arrow left" alt="" onClick={()=>handleClick("left")} />
+      <img src="assets/arrow.png" className="arrow right" alt="" onClick={()=>handleClick("right")}/>
     </div>
   )
 }
